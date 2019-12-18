427f072b51f86ca3945a4c3f95430303bbc02ad0
Kyrylo Maksymenko <saklar13@gmail.com>
2019-12-18 13:46:54 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+def read_file(file_name):
+    with open(file_name) as f:
+        return f.read()
+
+
+setup(
+    name='pip-download',
+    version=read_file('version.txt'),
+    description='downloads python packages for specified platform and python version',
+    author='Kyrylo Maksymenko',
+    author_email='saklar13@gmail.com',
+    url='https://github.com/saklar13/pip-download',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": ['pip-download = pip_download.cli:cli']
+    },
+    include_package_data=True,
+    install_requires=read_file('requirements.txt'),
+    python_requires='>=3.7',
+)
