497de31738637a7b577e37d93ef0af91a9c9388a
Kyrylo Maksymenko <saklar13@gmail.com>
2020-11-25 11:30:52 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 def read_file(file_name):
     with open(file_name) as f:
-        return f.read()
+        return f.read().strip()
 
 
 setup(
@@ -16,6 +16,12 @@ setup(
     packages=find_packages(),
     entry_points={"console_scripts": ["pip-download = pip_download.cli:cli"]},
     include_package_data=True,
-    install_requires=read_file("requirements.txt"),
+    install_requires=[
+        "click~=7.1",
+        "pip-tools~=5.4",
+        "pip>=20.1,<20.4",
+        "tqdm~=4.53",
+        "packaging~=20.4",
+    ],
     python_requires="~=3.7",
 )
